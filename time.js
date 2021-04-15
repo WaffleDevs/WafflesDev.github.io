e79ce691c319167e5fab30f8b6cfdbd0551d7698
WafflesDev <77215539+WafflesDev@users.noreply.github.com>
2021-04-14 23:59:02 -0400

    Add files via upload

---
+++ time.js
@@ -0,0 +1,44 @@
+//import 'bootstrap@4.6.0'
+//import $ from 'jquery'
+var time = 0;
+var timerRunning = false;
+
+loop()
+
+$('button')
+  .html('End') // Try edit it...
+  .on('click', () => end(false) + console.log('Count Paused:' + timerRunning))
+
+$('button2')
+  .html('Start') // Try edit it...
+  .on('click', () => end(true) + console.log('Count Started:' + timerRunning))
+
+async function loop()
+{
+    timer()
+    $("time").text('Time: ' + time);
+    $("timerRunning").text('timerRunning: ' + timerRunning);
+    await sleep(1000)
+    loop()
+}
+
+function end(state)
+{
+  timerRunning = state
+}
+
+function timer()
+{
+  if( timerRunning === true )
+  {
+    if( time < 200)
+    {  
+      time = time + 1
+      console.log(time)
+      
+    }
+  }
+}
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
