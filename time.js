1776d6bb362ee9d1a44747b809bffa00d9bb5cb0
WafflesDev <77215539+WafflesDev@users.noreply.github.com>
2021-04-22 03:01:09 -0400

    Update time.js

--- time.js
+++ time.js
@@ -1,8 +1,7 @@
-//import 'bootstrap@4.6.0'
-//import $ from 'jquery'
 var time = 0;
 var timerRunning = false;
-var xTime
+var xTime = 1;
+var timecap = 99999999999999999999999; // makes sure the numbers doent go for to long when a sleep error occurs
 
 loop()
 
@@ -16,13 +15,13 @@ $("#b3")
   .html('Restart')
   .on('click', () => console.log('Count Restarted.') + timeSet(1) + textUpdate())
 $("#b4")
-  .html('+1')
+  .html('+' + xTime)
   .on('click', () => timeSet(2) + textUpdate())
 $("#b5")
-  .html('-1')
+  .html('-' + xTime)
   .on('click', () => timeSet(3) + textUpdate())
 $("#b6")
-  .html('-1')
+  .html('Change Multiplier: ' + xTime)
   .on('click', () => timeMulti() )
   
 async function loop() {
@@ -33,24 +32,37 @@ async function loop() {
 }
 function timeMulti()
 {
-	
+	if (xTime == 1 || xTime == 10) {xTime = xTime * 10}
+  else if (xTime == 100) {xTime = 1}
+  console.log(' Multi: ' + xTime)
+  $("#b5")
+  .html('-' + xTime)
+  $("#b4")
+  .html('+' + xTime)
+  $("#b6")
+  .html('Change Multiplier: ' + xTime)
 }
 async function timeSet(operation) {
-  if (operation === 1) {time = 0, timerRunning = false}
-  if (operation === 2) {time = time + 1}
-  if (operation === 3) {time = time - 1}
+  if (operation === 1) {time = 0 && timerRunning == false}
+  if (operation === 2) {time = time + xTime}
+  if (operation === 3) {time = time - xTime}
 }
 function textUpdate() {
-  $("time").text('Time: ' + time);
+	var timeInM = Math.floor(time / 60);
+ 	var timeInH = Math.floor(time / 3600);
+ 	var timeInD = Math.floor(time / 86400);
+  $("time").text('Time:');
   $("timerRunning").text('timerRunning: ' + timerRunning);
+  $("smh").text('Days: ' + timeInD + '.  Hours: ' + timeInH + '.  Minutes: ' + timeInM + '.  Seconds: ' + time);
+  
 }
 function counter(value) {
   timerRunning = value
 }
 function timer() {
   if (timerRunning === true) {
-    if (time < 9999) {
-      time = time + 1
+    if (time < timecap) {
+      time++
       console.log(time)
       }}}
 function sleep(ms) {
